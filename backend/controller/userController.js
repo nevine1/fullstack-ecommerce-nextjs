29b6d79a3f26407863b338f20420dad0709291aa
nevine1 <venasamaan@gmail.com>
2025-10-24 20:41:06 -0400

    backend code for register new user is done

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -11,7 +11,32 @@ const registerUser = async (req, res) => {
           
         }
 
+        const existingUser = await User.find({ email });
+        if (existingUser) {
+            return res.json({
+                success: false, 
+                message: "This user is already existing."
+            })
+        }
 
+        const salt = await bcrypt.generateSale(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+       const  hashedConfirmPassword = await bcrypt.hash(confirmPassword, salt)
+        const newUserData = {
+            name, 
+            email, 
+            password: hashedPassword, 
+            confirmPassword: hashedConfirmPassword, 
+        }
+        const newUser = new User(newUserData);
+        await newUser.save();
+        const token = await jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+        console.log('token is:', token)
+        return res.json({
+            success: true,
+            message: "New user has successfully registered!", 
+            dat: token
+        })
     } catch (err) {
         return res.json({
             success: false, 
