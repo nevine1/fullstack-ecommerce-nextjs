76113d5c90b96e2970146a4e936d83f15571869a
nevine1 <venasamaan@gmail.com>
2025-10-31 21:03:27 -0400

    creating  usersSlice and users actions are done

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit"
 import usersReducer from './slices/usersSlice'
-import doctorsReducer from './slices/doctorsSlice'
-import appointmentsReducer from "./slices/appointmentsSlice"
+
 
 
 const rootReducer = combineReducers({
    users: usersReducer,
-   doctors: doctorsReducer, 
-   appointments: appointmentsReducer
+   
   
 })
   
--- frontend/src/store/slices/usersSlice.js
+++ frontend/src/store/slices/usersSlice.js
@@ -4,10 +4,31 @@ const usersSlice = createSlice({
     name: "users", 
     initialState: {
         users: [], 
+        userInfo: null,
+        token: null,
         isLoading: false, 
         error: null
     }, 
     reducers: {
-        
+        setLoading: (state, action) => {
+            state.isLoading = action.payload;
+        },
+        setUserInfo: (state, action) => {
+            state.userInfo = action.payload
+        },
+        getUsers: (state, action) => {
+            state.users = action.payload
+        }, 
+        setToken: (state, action) => {
+            state.token = action.payload;
+        }, 
+        uerLogout: (state, action) => {
+            state.userInfo = null;
+            state.token = null
+        }
+
     }
 })
+
+export const {setLoading,  getUsers , setToken ,setUserInfo,  uerLogout } = usersSlice.actions; 
+export default usersSlice.reducer;
