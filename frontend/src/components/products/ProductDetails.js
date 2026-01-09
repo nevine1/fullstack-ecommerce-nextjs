1cd32b6ea741a4e811346579248f2099d1b8853d
nevine <venasamaan@gmail.com>
2026-01-08 22:27:31 -0500

    continue working on product details

--- frontend/src/components/products/ProductDetails.js
+++ frontend/src/components/products/ProductDetails.js
@@ -1,17 +1,19 @@
 "use client"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
 import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux'
 import Image from 'next/image'
 import { setProductDetails, setIsLoading } from '../../store/slices/productsSlice'
+import { FaStar } from "react-icons/fa";
+import { FaStarHalfStroke } from "react-icons/fa6";
 
 const ProductDetails = () => {
     const { productInfo, isLoading } = useSelector((state) => state.products)
     const dispatch = useDispatch()
     const { id } = useParams()
     const backUrl = process.env.NEXT_PUBLIC_BACKEND_URL
-
+    const [imgValue, setImageVAlue] = useState(productInfo?.images[1])
     const fetchProductDetails = async () => {
         try {
             dispatch(setIsLoading(true))
@@ -41,31 +43,57 @@ const ProductDetails = () => {
             <div>
                 <h1>{productInfo?.name}</h1>
             </div>
-            <div className="min-h-[200px]">
+            <div className="min-h-[200px] flex md:flex-row sm:flex-col gap-8">
                 {/* images */}
-                <div className="h-94 flex flex-col lg:flex-row-reverse gap-6">
-                    <div className="h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-400">
+                <div className="h-94 flex flex-col lg:flex-row gap-6 mt-6">
 
-                    </div>
 
-                    <div className="flex lg:flex-col gap-4 overflow-scroll scrollbar-none h-full">
+                    <div className="flex lg:flex-col gap-4 overflow-scroll  h-full ">
                         {productInfo?.images?.map((img, i) => (
                             <div
                                 key={i}
-                                className="h-20 w-20 bg-gray-200 rounded-md"
+                                className="h-20 w-20 bg-gray-200 mb-4 rounded-md"
                             >
                                 <Image
                                     src={img}
                                     alt="product image"
                                     width={80}
                                     height={80}
-                                    className="object-cover rounded-md"
+                                    className="object-cover rounded-md cursor-pointer"
+                                    onClick={() => setImageVAlue(img)}
+                                    onMouseOver={() => setImageVAlue(img)}
                                 />
                             </div>
                         ))}
                     </div>
+                    <div className="h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-400">
+                        <Image
+                            src={imgValue || productInfo?.images[1]}
+                            alt="product image"
+                            width={300}
+                            height={300}
+                            className="w-full h-full object-cover rounded-md "
+                        />
+                    </div>
 
-
+                </div>
+                {/* product info */}
+                <div className="mx-4 flex flex-col gap-2">
+                    <p className="font-semibold mt-6 w-fit rounded-full bg-orange-200 px-4 py-1 text-orange-500">{productInfo?.brandName}</p>
+                    <p className=" font-semibold">{productInfo?.name}</p>
+                    <p className="text-[15px] text-slate-400 ">{productInfo?.category}</p>
+                    <div className="flex flex-row items-center  text-yellow-500">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStarHalfStroke />
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <p className="text-orange-500 font-semibold text-[18px]">${productInfo?.price}</p>
+                        <p className="text-slate-400 line-through font-semibold text-[18px]">${productInfo?.sellingPrice}</p>
+                    </div>
+                    <p className="text-[15px] text-slate-600 ">{productInfo?.description}</p>
                 </div>
 
             </div>
